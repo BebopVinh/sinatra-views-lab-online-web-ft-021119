c83a8824e9d52f0e69615bd7b0e9694e390f40d0
Vinh Do <vinhdo07@gmail.com>
2019-04-01 02:44:26 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require_relative './app'
+require_relative '../app'
 require_all '../views'
